43114151fed67a9218edd2c70b64bdaea6f23161
Mike <mcarmody@productiveedge.com>
2020-02-04 11:38:16 -0500

    first push

---
+++ ONI Test Mod/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+
+namespace ONI_Test_Mod
+{
+    [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+    internal class HelloWorld_GeneratedBuildings_LoadGeneratedBuildings
+    {
+        private static void Postfix()
+        {
+            Debug.Log("Hello, world!");
+        }
+    }
+}
